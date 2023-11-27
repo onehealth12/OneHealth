4eba7670434d969cf0b66ec12949e0b752024046
giankyle989 <kylelaguerta989@gmail.com>
2023-11-27 20:54:10 +0800

    Added Frontend domain to backend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://onehealth-frontend.onrender.com',
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
@@ -44,7 +46,7 @@ const server = app.listen(port, () => {
 
 io = require ('socket.io')(server, {
   cors: {
-    origin: '*',
+    origin: 'https://onehealth-frontend.onrender.com'
   },
 })
 
